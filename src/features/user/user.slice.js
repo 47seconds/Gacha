77607aaa0c496e09454ecbcfc28449f50c427b0a
47seconds <vaibhavlakshkar.12345@gmail.com>
2024-07-13 20:42:07 +0530

    configured user slice and reducers

---
+++ src/features/user/user.slice.js
@@ -0,0 +1,42 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialUserState = {
+    user: {
+        fullName: '',
+        username: '',
+        email: '',
+        avatarUrl: '',
+        coverImageUrl: '',
+        watchHistory: [],
+    },
+    isLogin: false
+}
+
+export const userSlice = createSlice({
+    name: 'user',
+    initialState: initialUserState,             
+    reducers: {
+        userLogin: (state, action) => {        
+            state.user.fullName= action.payload.fullName,
+            state.user.username= action.payload.username,
+            state.user.email= action.payload.email,
+            state.user.avatarUrl= action.payload.avatarUrl,
+            state.user.coverImageUrl= action.payload.coverImageUrl,
+            state.user.watchHistory= action.payload.watchHistory,
+            state.isLogin = true
+        },
+        userLogout: (state) => {
+            state.user.fullName= '',
+            state.user.username= '',
+            state.user.email= '',
+            state.user.avatarUrl= '',
+            state.user.coverImageUrl= '',
+            state.user.watchHistory= [],
+            state.isLogin = false
+        },
+    }
+});
+
+export const {userLogin, userLogout} = userSlice.actions;
+
+export default userSlice.reducer;
