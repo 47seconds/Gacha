2ba31a2e6f31e66c940b86b1db42a29f034fcb32
47seconds <vaibhavlakshkar.12345@gmail.com>
2024-07-13 20:37:33 +0530

    configured redux

---
+++ src/apps/store.app.js
@@ -0,0 +1,8 @@
+import {configureStore} from '@reduxjs/toolkit';
+import userReducer from '../features/user/user.slice.js';
+
+export const store = configureStore({
+    reducer: {
+        userReducer
+    }
+});
