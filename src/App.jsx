1018793d079762274212aae2183d895583d64641
47seconds <vaibhavlakshkar.12345@gmail.com>
2024-07-13 20:36:12 +0530

    added outlet for routing pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import React from "react";
+import {Outlet} from 'react-router-dom';
+import {Header, Footer} from './layouts'
+
 function App() {
-    return <></>;
+    return (
+    <div className="flex flex-col h-screen w-full">
+        {/* <TestForm/> */}
+        <Header/>
+        <main className="flex-1 overflow-auto flex-grow mt-16">
+            <Outlet/>
+        </main>
+        <Footer/>
+    </div>
+    );
 }
 
 export default App;
