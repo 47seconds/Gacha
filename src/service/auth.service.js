1226e82983993dcfc4c4ddc533de0f5fa4fb6cfa
47seconds <vaibhavlakshkar.12345@gmail.com>
2024-07-14 04:55:50 +0530

    added signup and delete functionalities

--- src/service/auth.service.js
+++ src/service/auth.service.js
@@ -4,7 +4,11 @@ export class AuthService {
     currentUser = async () => {
         try {
             const user = await axiosInstance.get('/users/me');
-            return user.data;
+            if(user.data.statusCode == 200) {
+                return res.data.data;
+            } else {
+                console.log('failed to account info');
+            }
         } catch (error) {
             throw error;
         }
@@ -20,7 +24,12 @@ export class AuthService {
                     password: userLoginData.password || ''
                 }
             );
-            return user.data;
+            // console.log(user.data)
+            if(user.data.statusCode == 200) {
+                return user.data;
+            } else {
+                console.log('cannot login');
+            }
         } catch (error) {
             throw error;
         }
@@ -28,12 +37,40 @@ export class AuthService {
 
     userLogout = async() => {
         try {
-            const res = await axiosInstance.post('/users/logout');
-            return res.data.message;
+            const res = await axiosInstance.post('/users/logout').then();
+            if(res.data.statusCode == 200) {
+                return res.data.message;
+            }
         } catch (error) {
             throw error;
         }
     }
+
+    userSignup = async(userSignupFormData) => {
+        try {
+            const res = await axiosInstance.post('/users/register', userSignupFormData, {
+                headers: {
+                  'Content-Type': 'multipart/form-data', // Important for Axios to correctly handle multipart data
+                },  
+              });
+            if(res.data.statusCode == 200) {
+                return res.data;
+            }
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    userDelete = async() => {
+        try {
+            const res = await axiosInstance.delete('/users/delete-account');
+            if(res.data.statusCode == 200) {
+                return res.data;
+            }
+        } catch (error) {
+            throw error;
+        }
+    } 
 }
 
 const authService = new AuthService();
