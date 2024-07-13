a425ddbad7cc222e08181b6be7d9e9c7bdcab08f
47seconds <vaibhavlakshkar.12345@gmail.com>
2024-07-14 04:46:48 +0530

    exported profile topbar avatar and user delete component

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,9 @@
 import LogoutButton from './LogoutButton';
+import ProfileAfterLogin from './ProfileAfterLogin';
+import DeleteUser from './DeleteUser';
 
 export {
     LogoutButton,
+    ProfileAfterLogin,
+    DeleteUser,
 }
