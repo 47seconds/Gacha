3ac4dded78f1f640611cc784cd1b482d9e32d3cb
47seconds <vaibhavlakshkar.12345@gmail.com>
2024-07-27 11:47:08 +0530

    routed land page to home as default

--- src/routes/NavRoutes.jsx
+++ src/routes/NavRoutes.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import {createBrowserRouter, createRoutesFromElements, Route, Navigate} from 'react-router-dom';
 import App from '../App';
-import {Home, SignIn, SignUp} from '../pages';
+import {Home, SignIn, SignUp, About} from '../pages';
 
 const router = createBrowserRouter(
     createRoutesFromElements(
         <Route path='/' element={<App/>}>
-            {/* <Route path='' element={<Navigate to='/home' replace/>}/> */}
+            <Route path='' element={<Navigate to='/home' replace/>}/>
             <Route path='home' element={<Home/>}/>
+            <Route path='about' element={<About/>}/>
             <Route path='signin' element={<SignIn/>}/>
             <Route path='signup' element={<SignUp/>}/>
         </Route>
