184c5e4421451e5f97d08a467bd86190aa86bee0
47seconds <vaibhavlakshkar.12345@gmail.com>
2024-07-27 09:10:55 +0530

    disable react dev tools on production site

--- src/main.jsx
+++ src/main.jsx
@@ -5,8 +5,9 @@ import { Provider } from "react-redux";
 import {store} from './apps/store.app.js';
 import router from './routes/NavRoutes.jsx';
 import {RouterProvider} from 'react-router-dom';
+import { disableReactDevTools } from "@fvilers/disable-react-devtools";
 
-
+if (process.env.NODE_ENV === 'production') disableReactDevTools();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <Provider store={store}>
