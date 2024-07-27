7fc2d439d95a69675a745fe9a73ab534e9d0176c
47seconds <vaibhavlakshkar.12345@gmail.com>
2024-07-27 09:32:16 +0530

    added production api server

--- src/conf/axiosInstance.conf.js
+++ src/conf/axiosInstance.conf.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const axiosInstance = axios.create({
         // baseURL: import.meta.env.VITE_GACHA_API_URL,
-        baseURL: '',
+        baseURL: 'https://api-gacha.onrender.com',
         withCredentials: true,
     });
 
