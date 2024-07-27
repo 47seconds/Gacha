7a24ea24eb8fa743bc4565378075075c2a4be6b7
47seconds <vaibhavlakshkar.12345@gmail.com>
2024-07-27 09:12:21 +0530

    add production api address

--- src/conf/axiosInstance.conf.js
+++ src/conf/axiosInstance.conf.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-        baseURL: import.meta.env.VITE_GACHA_API_URL,
+        // baseURL: import.meta.env.VITE_GACHA_API_URL,
+        baseURL: '',
         withCredentials: true,
     });
 
