e40d5ed27330e65a06fcc93ff12efdc3d76760f5
47seconds <vaibhavlakshkar.12345@gmail.com>
2024-07-27 05:50:22 +0530

    added Sidebar and Videoplayer

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,13 @@
 import LogoutButton from './LogoutButton';
 import ProfileAfterLogin from './ProfileAfterLogin';
 import DeleteUser from './DeleteUser';
+import SideBar from './SideBar';
+import VideoPlayer from './VideoPlayer';
 
 export {
     LogoutButton,
     ProfileAfterLogin,
     DeleteUser,
+    SideBar,
+    VideoPlayer,
 }
