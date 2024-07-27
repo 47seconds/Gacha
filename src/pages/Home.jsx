cfbb0ebda1657c9eb4c941cfcb5b435ee0c0b665
47seconds <vaibhavlakshkar.12345@gmail.com>
2024-07-27 05:56:45 +0530

    implemented videojs for shotties

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,70 @@
-import React from 'react'
+import React, { useRef, useState } from 'react';
+import {VideoPlayer} from '../components';
+import {useLatestShotties} from '../hooks/video';
+import 'video.js/dist/video-js.css';
 
 function Home() {
+  const videoPlayer = useRef(null);
+  const [shotties, setShotties] = useLatestShotties();
+
+  // Video.js player options
+  const VideoPlayerOptions = {
+    controls: true,
+    responsive: true,
+    fluid: true,
+    sources: [
+      {
+        src: shotties,
+        type: "application/x-mpegURL",
+      },
+    ],
+    loop: true,
+    auto: true,
+    aspectRatio: "9:16",
+    // autoplay: "any",
+  };
+
+  // Handle player readiness
+  const handlePlayerReady = (player) => {
+    videoPlayer.current = player;
+
+    // Event handling for player
+    player.on("waiting", () => {
+      videojs.log("player is waiting");
+    });
+
+    player.on("dispose", () => {
+      videojs.log("player will dispose");
+    });
+  };
+
   return (
-    <div>Home</div>
-  )
+    <div className='flex flex-col justify-center items-center'>
+      {shotties.map((url, index) => {
+          // Video.js player options
+          const VideoPlayerOptions = {
+            controls: true,
+            responsive: true,
+            fluid: true,
+            sources: [
+              {
+                src: url,
+                type: "application/x-mpegURL",
+              },
+            ],
+            loop: true,
+            aspectRatio: "9:16",
+            // autoplay: "any",
+      };
+
+      return (
+        <div key={index} className='mb-4'>
+          <VideoPlayer options={VideoPlayerOptions} onReady={handlePlayerReady}/>
+        </div>
+      )
+      })}
+    </div>
+  );
 }
 
-export default Home
+export default Home;
