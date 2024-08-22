91763a3954dc0d9412139cd84ab742483756ffcf
47seconds <vaibhavlakshkar.12345@gmail.com>
2024-08-22 17:58:28 +0530

    added a Loader for Signup

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useDispatch } from "react-redux";
 import authService from "../service/auth.service.js";
+import Loader from '../utils/Loader.util';
 import { userSignup } from "../features/user/user.slice.js";
 import { useNavigate } from "react-router-dom";
 
@@ -11,11 +12,13 @@ function SignUp() {
     const [email, setEmail] = useState("");
     const [avatar, setAvatar] = useState(undefined);
     const [coverImage, setCoverImage] = useState(undefined);
+    const [loader, setLoader] = useState(false);
     const dispach = useDispatch();
     const navigate = useNavigate();
 
     const signupHandler = async (e) => {
         e.preventDefault();
+        setLoader(true);
 
         const signupFormData = new FormData();
 
@@ -30,21 +33,29 @@ function SignUp() {
         if (res.statusCode == 200) {
             dispach(userSignup(res.data));
             console.log("account created");
+            setLoader(false);
             navigate("/home");
+        } else {
+          console.log(res);
+          setLoader(false);
         }
     };
 
     return (
         <div className='h-full w-full flex justify-center items-center'>
-          <form className='flex w-full flex-col justify-center items-center'>
-            <input type='text' placeholder='Full Name' required value={fullName} onChange={e => setFullName(e.target.value)} className='bg-gray-200 sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 rounded-2xl px-3 py-1.5 my-2 text-2xl font-medium'/>
-            <input type='text' placeholder='Email' required value={email} onChange={e => setEmail(e.target.value)} className='bg-gray-200 sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 rounded-2xl px-3 py-1.5 my-2 text-2xl font-medium'/>
-            <input type='text' placeholder='username' required value={username} onChange={e => setUsername(e.target.value)} className='bg-gray-200 sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 rounded-2xl px-3 py-1.5 my-2 text-2xl font-medium'/>
-            <input type='password' placeholder='password' required value={password} onChange={e => setPassword(e.target.value)} className='bg-gray-200 sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 rounded-2xl px-3 py-1.5 my-2 text-2xl font-medium'/>
-            <input type='file' placeholder='Avatar' accept=".png, .jpg, .jpeg" required   onChange={e => setAvatar(e.target.files[0])} className='bg-gray-200 sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 rounded-2xl px-3 py-1.5 my-2 text-2xl font-medium'/>
-            <input type='file' placeholder='Cover Image' accept=".png, .jpg, .jpeg" onChange={e => setCoverImage(e.target.files[0])} className='bg-gray-200 sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 rounded-2xl px-3 py-1.5 my-2 text-2xl font-medium'/>
-            <button onClick={signupHandler} className='bg-green-200 rounded-2xl px-5 py-1.5 my-2 text-2xl font-medium'>Sign up</button>
-          </form>
+          {loader ? (
+            <Loader/>
+          ) : (
+            <form className='flex w-full flex-col justify-center items-center'>
+              <input type='text' placeholder='Full Name' required value={fullName} onChange={e => setFullName(e.target.value)} className='bg-gray-200 sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 rounded-2xl px-3 py-1.5 my-2 text-2xl font-medium'/>
+              <input type='text' placeholder='Email' required value={email} onChange={e => setEmail(e.target.value)} className='bg-gray-200 sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 rounded-2xl px-3 py-1.5 my-2 text-2xl font-medium'/>
+              <input type='text' placeholder='username' required value={username} onChange={e => setUsername(e.target.value)} className='bg-gray-200 sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 rounded-2xl px-3 py-1.5 my-2 text-2xl font-medium'/>
+              <input type='password' placeholder='password' required value={password} onChange={e => setPassword(e.target.value)} className='bg-gray-200 sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 rounded-2xl px-3 py-1.5 my-2 text-2xl font-medium'/>
+              <input type='file' placeholder='Avatar' accept=".png, .jpg, .jpeg" required   onChange={e => setAvatar(e.target.files[0])} className='bg-gray-200 sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 rounded-2xl px-3 py-1.5 my-2 text-2xl font-medium'/>
+              <input type='file' placeholder='Cover Image' accept=".png, .jpg, .jpeg" onChange={e => setCoverImage(e.target.files[0])} className='bg-gray-200 sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 rounded-2xl px-3 py-1.5 my-2 text-2xl font-medium'/>
+              <button onClick={signupHandler} className='bg-green-200 rounded-2xl px-5 py-1.5 my-2 text-2xl font-medium'>Sign up</button>
+            </form>
+          )}
         </div>
     );
 }
