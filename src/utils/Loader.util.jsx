e2edc727a59b461e7e1d67c7f23a1088fd5f2433
47seconds <vaibhavlakshkar.12345@gmail.com>
2024-08-22 18:49:31 +0530

    reduced loader size

--- src/utils/Loader.util.jsx
+++ src/utils/Loader.util.jsx
@@ -3,8 +3,7 @@ import React from 'react'
 function Loader() {
   return (
     <div class="flex-col gap-4 w-full flex items-center justify-center">
-    <div class="w-28 h-28 border-8 text-blue-400 text-4xl animate-spin border-gray-300 flex items-center justify-center border-t-blue-400 rounded-full">
-    </div>
+    <div class="w-16 h-16 border-8 text-blue-400 text-4xl animate-spin border-gray-300 flex items-center justify-center border-t-blue-400 rounded-full"></div>
     </div>
   )
 }
