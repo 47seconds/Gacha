f29fcbdfbf37232057786301ec2184c3dea741c5
47seconds <vaibhavlakshkar.12345@gmail.com>
2024-08-22 17:27:11 +0530

    added Loading screen when fetching data from backend and removed unused variables

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import {VideoPlayer} from '../components';
+import ContentLoading from '../utils/ContentLoading.util';
 import {useLatestShotties} from '../hooks/video';
 import videojs from 'video.js';
 import 'video.js/dist/video-js.css';
@@ -7,29 +8,27 @@ import 'video.js/dist/video-js.css';
 function Home() {
   const videoPlayer = useRef(null);
   const [shotties, setShotties] = useLatestShotties();
+  // console.log(shotties);
+  const [contentLoading, setContentLoading] = useState(true);
+  const [fadeOut, setFadeOut] = useState(false);
 
-  // Video.js player options
-  const VideoPlayerOptions = {
-    controls: true,
-    responsive: true,
-    fluid: true,
-    sources: [
-      {
-        src: shotties,
-        type: "application/x-mpegURL",
-      },
-    ],
-    loop: true,
-    auto: true,
-    aspectRatio: "9:16",
-    // autoplay: "any",
-  };
+  useEffect(() => {
+    if (shotties.length > 0) {
+      // Start fade out when content is ready
+      setFadeOut(true);
 
-  // Handle player readiness
+      // Remove splash screen after fade-out completes
+      const timer = setTimeout(() => {
+        setContentLoading(false);
+      }, 1000); // Match with the duration of your fade-out transition
+
+      return () => clearTimeout(timer);
+    }
+  }, [shotties]);
+
   const handlePlayerReady = (player) => {
     videoPlayer.current = player;
 
-    // Event handling for player
     player.on("waiting", () => {
       videojs.log("player is waiting");
     });
@@ -41,29 +40,32 @@ function Home() {
 
   return (
     <div className='flex flex-col justify-center items-center'>
-      {shotties.map((url, index) => {
-          // Video.js player options
-          const VideoPlayerOptions = {
-            controls: true,
-            responsive: true,
-            fluid: true,
-            sources: [
-              {
-                src: url,
-                type: "application/x-mpegURL",
-              },
-            ],
-            loop: true,
-            aspectRatio: "9:16",
-            // autoplay: "any",
-      };
+      {contentLoading ? (
+        <ContentLoading fadeOut={fadeOut}/>
+      ) : (
+        shotties.map((url, index) => {
+            const VideoPlayerOptions = {
+              controls: true,
+              responsive: true,
+              fluid: true,
+              sources: [
+                {
+                  src: url,
+                  type: "application/x-mpegURL",
+                },
+              ],
+              loop: true,
+              aspectRatio: "9:16",
+              // autoplay: "any",
+        };
 
-      return (
-        <div key={index} className='mb-4'>
-          <VideoPlayer options={VideoPlayerOptions} onReady={handlePlayerReady}/>
-        </div>
-      )
-      })}
+        return (
+          <div key={index} className='mb-4'>
+            <VideoPlayer options={VideoPlayerOptions} onReady={handlePlayerReady}/>
+          </div>
+        )
+        })
+      )}
     </div>
   );
 }
