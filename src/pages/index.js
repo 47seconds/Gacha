dfde8bd1337c92d89ef96f891d76b96968b4a480
47seconds <vaibhavlakshkar.12345@gmail.com>
2024-08-22 18:49:05 +0530

    added pages for uploading shotties and user settings

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,5 +2,7 @@ import Home from "./Home";
 import SignIn from "./SignIn";
 import SignUp from "./SignUp";
 import About from "./About";
+import AccountSettings from './AccountSettings';
+import UploadShottiePage from "./UploadShottiePage";
 
-export {Home, SignIn, SignUp, About}
+export {Home, SignIn, SignUp, About, AccountSettings, UploadShottiePage}
