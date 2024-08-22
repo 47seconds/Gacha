887de1c431201eba0eb65f9a21777f1a621ab310
47seconds <vaibhavlakshkar.12345@gmail.com>
2024-08-22 17:25:38 +0530

    added a Loading screen when backend server is booting

--- src/utils/ContentLoading.util.jsx
+++ src/utils/ContentLoading.util.jsx
@@ -4,7 +4,7 @@ function ContentLoading({fadeOut}) {
   return (
     <div className={`fixed inset-0 flex items-center justify-center bg-gray-900 transition-opacity duration-1000 ${fadeOut ? 'opacity-0' : 'opacity-100'}`}>
         <div>
-          <div className="text-3xl text-yellow-100 font-bold animate-pulse">Booting up backend server...</div>
+          <div className="text-3xl text-yellow-100 font-bold animate-pulse">Booting up backend server. This may take a while...</div>
           <div className="mt-10 text-red-400">
             <p>Due to free tier constrains, backend server shuts down if there is no traffic on the site.</p>
             <p>Please refresh page in few moments.</p>
