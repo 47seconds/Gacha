533c85a381bbed0a59b641a68d4c8a7bb82d6234
47seconds <vaibhavlakshkar.12345@gmail.com>
2024-08-22 17:20:01 +0530

    fixed input box responsiveness

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -37,8 +37,8 @@ function SignIn() {
     return (
         <div className='w-full h-full flex justify-center items-center'>
           <form className='w-full h-1/2 flex flex-col justify-center items-center p-5 rounded-2xl'>
-            <input type='text' placeholder='username or email' className='rounded-2xl font-medium text-xl m-2 p-2 bg-gray-200 w-3/4' value={username} onChange={(e) => setUsername(e.target.value)}/>
-            <input type='password' placeholder='password' className='rounded-2xl font-medium text-xl p-2 m-2 bg-gray-200 w-3/4' value={password} onChange={(e) => setPassword(e.target.value)}/>
+            <input type='text' placeholder='username or email' className='rounded-2xl font-medium text-xl m-2 p-2 bg-gray-200 sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5' value={username} onChange={(e) => setUsername(e.target.value)}/>
+            <input type='password' placeholder='password' className='rounded-2xl font-medium text-xl p-2 m-2 bg-gray-200 sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5' value={password} onChange={(e) => setPassword(e.target.value)}/>
             <button type='submit' className='rounded-2xl bg-green-400 font-medium text-xl text-white no-underline mt-10 px-3 py-1' onClick={loginHandler}>Login</button>
           </form>
         </div>
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -37,12 +37,12 @@ function SignUp() {
     return (
         <div className='h-full w-full flex justify-center items-center'>
           <form className='flex w-full flex-col justify-center items-center'>
-            <input type='text' placeholder='Full Name' required value={fullName} onChange={e => setFullName(e.target.value)} className='bg-gray-200 w-3/4 rounded-2xl px-3 py-1.5 my-2 text-2xl font-medium'/>
-            <input type='text' placeholder='Email' required value={email} onChange={e => setEmail(e.target.value)} className='bg-gray-200 w-3/4 rounded-2xl px-3 py-1.5 my-2 text-2xl font-medium'/>
-            <input type='text' placeholder='username' required value={username} onChange={e => setUsername(e.target.value)} className='bg-gray-200 w-3/4 rounded-2xl px-3 py-1.5 my-2 text-2xl font-medium'/>
-            <input type='password' placeholder='password' required value={password} onChange={e => setPassword(e.target.value)} className='bg-gray-200 w-3/4 rounded-2xl px-3 py-1.5 my-2 text-2xl font-medium'/>
-            <input type='file' placeholder='Avatar' accept=".png, .jpg, .jpeg" required   onChange={e => setAvatar(e.target.files[0])} className='bg-gray-200 w-3/4 rounded-2xl px-3 py-1.5 my-2 text-2xl font-medium'/>
-            <input type='file' placeholder='Cover Image' accept=".png, .jpg, .jpeg" onChange={e => setCoverImage(e.target.files[0])} className='bg-gray-200 w-3/4 rounded-2xl px-3 py-1.5 my-2 text-2xl font-medium'/>
+            <input type='text' placeholder='Full Name' required value={fullName} onChange={e => setFullName(e.target.value)} className='bg-gray-200 sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 rounded-2xl px-3 py-1.5 my-2 text-2xl font-medium'/>
+            <input type='text' placeholder='Email' required value={email} onChange={e => setEmail(e.target.value)} className='bg-gray-200 sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 rounded-2xl px-3 py-1.5 my-2 text-2xl font-medium'/>
+            <input type='text' placeholder='username' required value={username} onChange={e => setUsername(e.target.value)} className='bg-gray-200 sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 rounded-2xl px-3 py-1.5 my-2 text-2xl font-medium'/>
+            <input type='password' placeholder='password' required value={password} onChange={e => setPassword(e.target.value)} className='bg-gray-200 sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 rounded-2xl px-3 py-1.5 my-2 text-2xl font-medium'/>
+            <input type='file' placeholder='Avatar' accept=".png, .jpg, .jpeg" required   onChange={e => setAvatar(e.target.files[0])} className='bg-gray-200 sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 rounded-2xl px-3 py-1.5 my-2 text-2xl font-medium'/>
+            <input type='file' placeholder='Cover Image' accept=".png, .jpg, .jpeg" onChange={e => setCoverImage(e.target.files[0])} className='bg-gray-200 sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5 rounded-2xl px-3 py-1.5 my-2 text-2xl font-medium'/>
             <button onClick={signupHandler} className='bg-green-200 rounded-2xl px-5 py-1.5 my-2 text-2xl font-medium'>Sign up</button>
           </form>
         </div>
