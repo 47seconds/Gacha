f8538658337846002119075438a7654d50ce1b18
47seconds <vaibhavlakshkar.12345@gmail.com>
2024-08-22 18:44:56 +0530

    added buttons for user account settings and shottie upload

--- src/components/index.js
+++ src/components/index.js
@@ -5,6 +5,8 @@ import SideBar from './SideBar';
 import VideoPlayer from './VideoPlayer';
 import AboutMe from './AboutMe';
 import Contact from './Contact';
+import UploadShottie from './UploadShottie';
+import UserSettings from './UserSettings';
 
 export {
     LogoutButton,
@@ -14,4 +16,6 @@ export {
     VideoPlayer,
     Contact,
     AboutMe,
+    UploadShottie,
+    UserSettings,
 }
