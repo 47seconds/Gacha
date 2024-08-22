9403df4fcbaf0b3e18f9b4f8e4a9dcdd65b2d0fe
47seconds <vaibhavlakshkar.12345@gmail.com>
2024-08-22 18:50:46 +0530

    added user account settings and shottie routes

--- src/routes/NavRoutes.jsx
+++ src/routes/NavRoutes.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import {createBrowserRouter, createRoutesFromElements, Route, Navigate} from 'react-router-dom';
 import App from '../App';
-import {Home, SignIn, SignUp, About} from '../pages';
+import {Home, SignIn, SignUp, About, AccountSettings, UploadShottiePage} from '../pages';
 
 const router = createBrowserRouter(
     createRoutesFromElements(
@@ -11,6 +11,8 @@ const router = createBrowserRouter(
             <Route path='about' element={<About/>}/>
             <Route path='signin' element={<SignIn/>}/>
             <Route path='signup' element={<SignUp/>}/>
+            <Route path='account-settings' element={<AccountSettings/>}/>
+            <Route path='upload/shottie' element={<UploadShottiePage/>}/>
         </Route>
     )
 )
