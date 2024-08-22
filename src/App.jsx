079c9563ffeb3010b43645359b5e91c285335a92
47seconds <vaibhavlakshkar.12345@gmail.com>
2024-08-22 17:17:24 +0530

    added a splash screen on app launch

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,33 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import {Outlet} from 'react-router-dom';
 import {Header, Footer} from './layouts'
+import  SplashScreen  from "./utils/SplashScreen.util";
 
 function App() {
+    const [isLoading, setIsLoading] = useState(true);
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setIsLoading(false);
+        }, 2000);
+
+        return () => clearTimeout(timer);
+    }, []);
+
     return (
     <div className="flex flex-col h-screen w-full">
-        <Header/>
-        <main className="flex-1 overflow-auto flex-grow bg-gray-900">
-            <Outlet/>
-        </main>
-        {/* <Footer/> */}
+        {
+            isLoading ? (
+                <SplashScreen/>
+            ) : (
+                <>
+                    <Header/>
+                    <main className="flex-1 overflow-auto flex-grow bg-gray-900">
+                        <Outlet/>
+                    </main>
+                    {/* <Footer/> */}
+                </>
+            )
+        }
     </div>
     );
 }
